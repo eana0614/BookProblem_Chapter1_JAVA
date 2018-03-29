9107c930c673aa4aad063023c2fb0dc20a3b6a92
EANA <EANA@EANA-DESKTOP>
2018-03-29 22:23:38 +0900

    Solve Fibonacci Numbers

--- Chapter1/src/FibonacciNumbers/Fibonacci.java
+++ Chapter1/src/FibonacciNumbers/Fibonacci.java
@@ -1,9 +1,34 @@
 package FibonacciNumbers;
 
+import java.util.Scanner;
+
+// F(n) = F(n-1) + F(n-2)
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("출력할 피보나치 수열의  끝 번호를 입력해주세요 : ");
+		int end = scanner.nextInt();
+		
+		int value1 = 1;
+		int value2 = 0;
+		
+		for(int i= 0; i<end; i++) {
+			
+			int valueN = value1 + value2;
+			
+			if(i % 5 == 0) {
+				System.out.println();
+			}
+			
+			System.out.print("("+(i+1)+") "+valueN+"\t");
+			
+			value2 = value1;
+			value1 = valueN;
+		}
 
 	}
 
