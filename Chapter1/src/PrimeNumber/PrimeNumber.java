fb9377334c9146cd39b4c741812898acd84d77ae
EANA <EANA@EANA-DESKTOP>
2018-03-29 23:43:44 +0900

    Solve Prime Number

--- Chapter1/src/PrimeNumber/PrimeNumber.java
+++ Chapter1/src/PrimeNumber/PrimeNumber.java
@@ -3,8 +3,40 @@ package PrimeNumber;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("1~1000 사이의 숫자 중 소수 구하기");
+		
+		int lineCount = 0;
+		int count = 0;
+		
+		for(int i=1; i<=1000; i++) {
+			if(isPrimeNum(i)) {
+				System.out.print(i+"\t");
+				lineCount++;
+				count++;
+				if(lineCount > 10) {
+					System.out.println();
+					lineCount = 0;
+				}
+			}
+		}
+		
+		System.out.println();
+		System.out.println("1~1000까지의 소수 갯수 : "+count);
+		
+	}
 
+	private static boolean isPrimeNum(int i) {
+		
+		boolean result = true;
+	
+		for(int j=2; j<i; j++) {
+			if(i%j == 0) {
+				result = false;
+			}
+		}
+		
+		return result;
 	}
 
 }
