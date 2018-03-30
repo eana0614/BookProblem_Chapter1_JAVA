0e1c37000077124c9011f701f6e9d8daeca377f3
EANA <EANA@EANA-DESKTOP>
2018-03-30 18:25:12 +0900

    Add function Recursive

--- Chapter1/src/GCD/GCD.java
+++ Chapter1/src/GCD/GCD.java
@@ -3,16 +3,18 @@ package GCD;
 import java.util.Scanner;
 
 public class GCD {
+	
+	static int count = 0;
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		int mode = -1;
 		
-		System.out.println("[1] 숫자의 약수 구하기, [2] 2개의 숫자에서 공통으로 해당하는 약수 구하기, [3] 2개의 숫자의 최대공약수 구하기, [0] 끝내기 ");
+		System.out.println("[1] 숫자의 약수 구하기, [2] 2개의 숫자에서 공통으로 해당하는 약수 구하기, [3] 2개의 숫자의 최대공약수 구하기, [4] 재귀함수로 최대공약수 구하기, [0] 끝내기 ");
 		mode = scanner.nextInt();
 		
-		while (mode > 0 && mode < 4) {
+		while (mode > 0 && mode < 5) {
 			
 			if(mode == 1) {
 				System.out.print("!] 숫자 1개를 입력해주세요 : ");
@@ -24,22 +26,40 @@ public class GCD {
 				int num1 = scanner.nextInt();
 				int num2 = scanner.nextInt();
 				findCDTwoNum(num1, num2);
-			}else {
+			}else if(mode == 3){
 				System.out.print("!] 숫자 2개를 입력해주세요 : ");
 				int num1 = scanner.nextInt();
 				int num2 = scanner.nextInt();
 				findGCDTWoNum(num1, num2);
+			}else {
+				System.out.print("!] 재귀함수로 최대 공약수를 구합니다. 숫자 2개를 입력해주세요. : ");
+				int num1 = scanner.nextInt();
+				int num2 = scanner.nextInt();
+				int result = findGDCUseRecursive(num1, num2);
+				System.out.println(count+"회 반복 : "+result);
 			}
 			
 			System.out.println();
-			System.out.println("[1] 숫자의 약수 구하기, [2] 2개의 숫자에서 공통으로 해당하는 약수 구하기, [3] 2개의 숫자의 최대공약수 구하기, [0] 끝내기 ");
+			System.out.println("[1] 숫자의 약수 구하기, [2] 2개의 숫자에서 공통으로 해당하는 약수 구하기, [3] 2개의 숫자의 최대공약수 구하기, [4] 재귀함수로 최대공약수 구하기, [0] 끝내기 ");
 			mode = scanner.nextInt();
 		}
 		
-		if(mode >=4) {
+		if(mode > 4) {
 			System.err.println("!] Range Input Error");
 		}else {
 			System.out.println("---------------------------");
+		}
+		
+	}
+
+	private static int findGDCUseRecursive(int num1, int num2) {
+		
+		count++;
+		
+		if(num2 != 0) {
+			return findGDCUseRecursive(num2, num2%num1);
+		}else {
+			return num1;
 		}
 		
 	}
@@ -55,7 +75,7 @@ public class GCD {
 			}
 		}
 		
-		System.out.println(result);
+		System.out.println("최대공약수 : "+result);
 	}
 
 	private static void findCDTwoNum(int num1, int num2) {
