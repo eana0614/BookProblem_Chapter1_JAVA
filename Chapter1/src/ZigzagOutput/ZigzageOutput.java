0e84906b7b5bd4d98b29fcac77d21b9579bc293c
EANA <EANA@EANA-DESKTOP>
2018-03-30 18:10:48 +0900

    Solve Zigzage Output

--- Chapter1/src/ZigzagOutput/ZigzageOutput.java
+++ Chapter1/src/ZigzagOutput/ZigzageOutput.java
@@ -1,9 +1,37 @@
 package ZigzagOutput;
 
 public class ZigzageOutput {
+	
+	static int MAX = 50;
+	static int ElementNum = 10;
 
 	public static void main(String[] args) {
 		
+		int lineNum = MAX/ElementNum;
+		
+		for(int i=1; i<=lineNum; i++) {
+			
+			if(i%2 != 0) {
+				printOddLine(i*ElementNum);
+			}else {
+				printEvenLine(i*ElementNum);
+			}
+			System.out.println();
+		}
+	}
+
+	private static void printEvenLine(int i) {
+		
+		for(int j=i; j>i-ElementNum; j--) {
+			System.out.print(j+"\t");
+		}
+	}
+
+	private static void printOddLine(int i) {
+		
+		for(int j=i-ElementNum+1; j<=i; j++) {
+			System.out.print(j+"\t");
+		}
 
 	}
 
